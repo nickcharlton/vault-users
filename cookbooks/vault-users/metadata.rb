073798f232ec0889c39c56e655e065fed1d5de09
Nick Charlton <hello@nickcharlton.net>
2013-09-21 18:21:59 +0100

    Adds a cookbook that creates system users from ChefVault data bags.

---
+++ cookbooks/vault-users/metadata.rb
@@ -0,0 +1,7 @@
+name             'vault-users'
+maintainer       'Nick Charlton'
+maintainer_email 'ops@nullgrid.net'
+license          'Apache 2.0'
+description      'Installs/Configures vault-users'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
