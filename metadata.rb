1dc6f00a556a492841ee8f9dd23e66cb72c1bcf4
Nick Charlton <hello@nickcharlton.net>
2013-11-01 20:10:15 +0000

    Bumps the release to 0.2.0.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name             'vault-users'
 maintainer       'Nick Charlton'
-maintainer_email 'ops@nullgrid.net'
+maintainer_email 'nick@nickcharlton.net'
 license          'Apache 2.0'
 description      'Installs/Configures vault-users'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'chef-vault'
