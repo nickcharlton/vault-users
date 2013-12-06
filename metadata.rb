8cc3edd1d3d15a443446a4081fb94e3be7184453
Nick Charlton <hello@nickcharlton.net>
2013-12-06 12:00:56 +0000

    Bumps the version to 0.2.4.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'nick@nickcharlton.net'
 license          'Apache 2.0'
 description      'Installs/Configures vault-users'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.3'
+version          '0.2.4'
 
 depends 'chef-vault'
